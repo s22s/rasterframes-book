294d43f5f56048b2c7089c92e3ba270e93616611
James Nachbar <jnachbar@astraea.io>
2018-08-08 14:21:32 -0400

    add dockerfile for server

--- conf.py
+++ conf.py
@@ -66,7 +66,7 @@ language = None
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
 # This pattern also affects html_static_path and html_extra_path .
-exclude_patterns = ['.build', 'Thumbs.db', '.DS_Store', '**.ipynb_checkpoints', 'UF-**']
+exclude_patterns = ['.build', 'Thumbs.db', '.DS_Store', '**.ipynb_checkpoints', '**/UF-**']
 
 # nbsphinx_execute = 'always'
 
